6c88c4e9cf4d6450317adf46caffc347e47eb84b
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-07-26 22:15:20 +0530

    Home Page and Signup, Login Pages Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./features/auth/components/Login";
+import SignupPage from "./pages/SignupPage";
+
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <Home/>
+      <LoginPage/>
+      {/* <SignupPage/> */}
     </div>
   );
 }
